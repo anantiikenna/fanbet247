f2440f5d4e340b04c8a005d41e806c877c016196
Ikenna <ananti.ikenna@gmail.com>
2024-11-04 09:21:08 +0100

    "Update layout styling and API URLs"

--- lib/api/apiUrls.ts
+++ lib/api/apiUrls.ts
@@ -1,5 +1,5 @@
 export const API_URLS = {
-    FETCH_UPCOMING_FIXTURES: 'https://backend.fanbet247.online/api/sports/upcoming-fixtures/',
-    FETCH_SINGLE_FIXTURE_DETAILS: (fixture_id: number) => `https://backend.fanbet247.online/api/sports/fixture/${fixture_id}`,
+    FETCH_UPCOMING_FIXTURES: 'https://backend.fanbet247.online/api/sports/upcoming-fixtures/?format=json',
+    FETCH_SINGLE_FIXTURE_DETAILS: (fixture_id: number) => `https://backend.fanbet247.online/api/sports/fixture/${fixture_id}/?format=json`,
 };
 
