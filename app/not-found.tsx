b1c81405b78da538bef5ff10e757396d3959a53e
Ikenna <ananti.ikenna@gmail.com>
2024-11-09 17:03:06 +0100

    created a custom  404 page.

---
+++ app/not-found.tsx
@@ -0,0 +1,24 @@
+
+import Footer from '@/components/sections/Footer';
+import Nav from '@/components/sections/Nav'
+import React from 'react'
+
+const NotFound= () => {
+  
+  return (
+    <div className=' w-full'>
+      <Nav/>
+      <div className='flex flex-col justify-center items-center h-screen'>
+        <div className='flex justify-between gap-5 items-center'>
+          <h1>404</h1>
+          <div className='h-14 border-x border-yellow-200'/>
+          <p>This page could not be found.</p>
+        </div>
+
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default NotFound
