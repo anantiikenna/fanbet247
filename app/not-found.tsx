78defa3c6d1fae33f1305603e7409f613dcebb74
Ikenna <ananti.ikenna@gmail.com>
2024-11-10 09:17:39 +0100

    "Added MobileNavbar import and component to NotFound page"

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,6 @@
 
 import Footer from '@/components/sections/Footer';
+import MobileNavbar from '@/components/sections/MobileNavbar';
 import Nav from '@/components/sections/Nav'
 import React from 'react'
 
@@ -8,6 +9,7 @@ const NotFound= () => {
   return (
     <div className=' w-full'>
       <Nav/>
+      <MobileNavbar/>
       <div className='flex flex-col justify-center items-center h-screen'>
         <div className='flex justify-between gap-5 items-center'>
           <h1>404</h1>
