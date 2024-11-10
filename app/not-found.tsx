0797b09a52deb2c50c886b9da4402d34aecd800f
Ikenna <ananti.ikenna@gmail.com>
2024-11-10 09:59:04 +0100

    "Updated NotFound component: added h-screen class, changed container div styles, and moved Footer to fixed bottom position."

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,18 +7,21 @@ import React from 'react'
 const NotFound= () => {
   
   return (
-    <div className=' w-full'>
+    <div className=' w-full h-screen'>
       <Nav/>
       <MobileNavbar/>
-      <div className='flex flex-col justify-center items-center h-screen'>
-        <div className='flex justify-between gap-5 items-center'>
+      <div className='flex flex-col justify-center items-center h-3/5 '>
+        <div className='flex justify-between gap-5 items-center '>
           <h1>404</h1>
           <div className='h-14 border-x border-yellow-200'/>
           <p>This page could not be found.</p>
         </div>
 
       </div>
-      <Footer/>
+      <div className='fixed bottom-0 w-full'>
+        <Footer/>
+      </div>
+      
     </div>
   );
 }
